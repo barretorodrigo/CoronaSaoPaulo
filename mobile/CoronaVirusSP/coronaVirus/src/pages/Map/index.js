de3ca897610da3bb115aa1f1035331cf77d11627
barretorodrigo <barretorodrigo@gmail.com>
2020-04-17 17:55:17 -0300

    Adding react-native-map, configuring api and display Sao Paulo map

--- mobile/CoronaVirusSP/coronaVirus/src/pages/Map/index.js
+++ mobile/CoronaVirusSP/coronaVirus/src/pages/Map/index.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import { View } from 'react-native';
 import Header from '../../Components/Header';
 import styles from './style';
@@ -7,6 +8,17 @@ export default function Map(){
     return(
         <View style={styles.container}>
             <Header/>
+            <MapView
+                provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+                style={styles.map}
+                region={{
+                    latitude: -23.5489,
+                    longitude: -46.6388,
+                    latitudeDelta: 7,
+                    longitudeDelta: 7,
+                }}
+                >
+            </MapView>
         </View>
     )
 }
--- mobile/CoronaVirusSP/coronaVirus/src/pages/Map/style.js
+++ mobile/CoronaVirusSP/coronaVirus/src/pages/Map/style.js
@@ -4,6 +4,15 @@ export default StyleSheet.create({
     container: {
         flex: 1,
         paddingTop: 20,
-        alignItems: 'center'
-    },
+        alignItems: 'center',
+        ...StyleSheet.absoluteFillObject,
+        height: 400,
+        width: 400,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+      },
+      map: {
+        ...StyleSheet.absoluteFillObject,
+        
+      },
 });
